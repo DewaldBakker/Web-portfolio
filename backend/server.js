740c397527783a5c6edc93f4dc0c1a3b9b3f5aa4
Dewald Bakker <601829@student.belgiumcampus.ac.za>
2026-06-01 10:43:45 +0200

    fix: fix email not send

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
-
-const app = express();
+import { Resend } from 'resend';
 
 // Configure CORS to grant smooth access to your Vercel deployment and local testing environment
 app.use(
